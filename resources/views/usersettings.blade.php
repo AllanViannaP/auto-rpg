bdee66694b1ab67d7cde20673028d8d0d9516068
AllanViannaP <erikacrt24@gmail.com>
2023-08-16 21:47:01 -0500

    User Settings page updated

--- resources/views/usersettings.blade.php
+++ resources/views/usersettings.blade.php
@@ -5,13 +5,22 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <h1>Settings</h1>
-                <div> Username: {{$user->name}} 
+                <h1>Account Settings</h1>
+                <div class="card">
+                    <div class="card-header"> My Profile </div>
+                    <div class="card-body">
+                        <center>
+                        <p>Username: {{$user->name}}</p>
+                        <p>Title: {{$user->titlepre}} {{$user->title}}</p>
+                        </center>
+                    </div> 
                 </div>
-                <div> Email: {{$user->email}}
-                </div>
-                
-
+                <br>
+                <div class="card">
+                    <div class="card-header"> Account info </div>
+                    <div class="card-body"> 
+                    <div> Email: {{$user->email}} </div>
+                    </div>
             </div>
         </div>
     </div>
