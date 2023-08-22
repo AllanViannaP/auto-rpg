6450130b5407bce3c267c3c6554a31b6f8d7b297
AllanViannaP <erikacrt24@gmail.com>
2023-08-22 18:27:26 -0300

    user settings cards

--- resources/views/usersettings.blade.php
+++ resources/views/usersettings.blade.php
@@ -7,20 +7,26 @@
             <div class="col-md-8">
                 <h1>Account Settings</h1>
                 <div class="card">
-                    <div class="card-header"> My Profile </div>
+                    <div class="card-header"> <h4>My Profile </h4>
+                    </div>
                     <div class="card-body">
-                        <center>
-                        <p>Username: {{$user->name}}</p>
-                        <p>Title: {{$user->titlepre}} {{$user->title}}</p>
-                        </center>
+                        <div id="profileinfo">
+                            <p>Username: {{$user->name}}</p>
+                            <p>Title: {{$user->titlepre}} {{$user->title}}</p>
+                        </div>
+                        <div id="profileedit">
+                            <form method="POST">
+                                
+                            </form>
+                            
                     </div> 
-                </div>
-                <br>
+                </div>  <br>
                 <div class="card">
                     <div class="card-header"> Account info </div>
                     <div class="card-body"> 
                     <div> Email: {{$user->email}} </div>
                     </div>
+                </div>
             </div>
         </div>
     </div>
