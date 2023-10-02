ea7481994d03bdaadd6ad3d667546e65ab3e9321
Allan Vianna <119679121+AllanViannaP@users.noreply.github.com>
2023-10-02 14:11:41 -0300

    max input size in name add

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -71,7 +71,7 @@
                         <div class="form-group row mt-4 justify-content-center">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
                             <div class="col-md-6">
-                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" maxlength="21" required autocomplete="name" autofocus>
                             @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
